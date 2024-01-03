f406190281ea79dbf1f2f9455c09473839ba7585
oleksandrgrebinskij <roki000sasha@gmail.com>
2024-01-03 13:02:47 +0200

    finish todolist

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,29 +1,30 @@
 import React, { useState } from 'react';
 
-const Input = () => {
-    const [task, setTask] = useState('');
+const Input = ({onSave}) => {
+    const [inputValue, setInputValue] = useState('');
 
     const handleInputChange = (e) => {
-        setTask(e.target.value);
+        setInputValue(e.target.value);
     };
 
-    const handleFormSubmit = (e) => {
-        e.preventDefault();
-        localStorage.setItem('task', task);
+    const handleSaveClick = () => {
+        onSave(inputValue);
+        setInputValue('');
     };
+
 
     return (
-        <form className='text-center font-custom pt-8 pb-9' onSubmit={handleFormSubmit}>
+        <form className='text-center font-custom pt-8 pb-9'>
             <input
                 className='px-3 rounded-l-lg placeholder:text-inputTextPlaceholderDark outline-none bg-darkDark text-inputTextDark w-[570px] h-[45px] text-xl'
                 name="inputToDO"
                 id="inputToDO"
                 placeholder='Add a task.'
                 type="text"
-                value={task}
+                value={inputValue}
                 onChange={handleInputChange}
             />
-            <button className='rounded-r-lg text-black bg-bronzeDark w-[130px] h-[45px] ' type="submit">
+            <button className='rounded-r-lg text-black bg-bronzeDark w-[130px] h-[45px] ' onClick={handleSaveClick} type="submit">
                 <p className='text-xl'>I will do it!</p>
             </button>
         </form>
